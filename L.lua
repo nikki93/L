e5873fd6c3d225c1c6533dc26f00fb41ab693226
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-06-21 13:26:05 -0700

    update lookpu behavior

--- L.lua
+++ L.lua
@@ -1,46 +1,14 @@
--- Main `L` table behavior -- look through Love modules, cache results
+-- Main lookup behavior
 
 local L = setmetatable({}, {
     __index = function(t, k)
         local r
-        if love.audio and love.audio[k]then
-            r = love.audio[k]
-        elseif love.data and love.data[k]then
-            r = love.data[k]
-        elseif love.event and love.event[k]then
-            r = love.event[k]
-        elseif love.filesystem and love.filesystem[k]then
-            r = love.filesystem[k]
-        elseif love.font and love.font[k]then
-            r = love.font[k]
-        elseif love.graphics and love.graphics[k]then
+        if love.graphics and love.graphics[k] then
             r = love.graphics[k]
-        elseif love.image and love.image[k]then
-            r = love.image[k]
-        elseif love.joystick and love.joystick[k]then
-            r = love.joystick[k]
-        elseif love.keyboard and love.keyboard[k]then
-            r = love.keyboard[k]
-        elseif love.math and love.math[k]then
-            r = love.math[k]
-        elseif love.mouse and love.mouse[k]then
-            r = love.mouse[k]
-        elseif love.physics and love.physics[k]then
-            r = love.physics[k]
-        elseif love.sound and love.sound[k]then
-            r = love.sound[k]
-        elseif love.system and love.system[k]then
-            r = love.system[k]
-        elseif love.thread and love.thread[k]then
-            r = love.thread[k]
-        elseif love.timer and love.timer[k]then
-            r = love.timer[k]
-        elseif love.touch and love.touch[k]then
-            r = love.touch[k]
-        elseif love.video and love.video[k]then
-            r = love.video[k]
-        elseif love.window and love.window[k]then
-            r = love.window[k]
+        elseif love[k] then
+            r = love[k]
+        elseif castle and castle[k] then
+            r = castle[k]
         end
         if r == nil then
             error("`L." .. k .. "` isn't defined")
@@ -49,48 +17,6 @@ local L = setmetatable({}, {
         return r
     end
 })
-
-
--- Disambiguate clashes
-
-L.compress = love.data and love.data.compress
-
-L.decompress = love.data and love.data.decompress
-
-L.getDPIScale = love.graphics and love.graphics.getDPIScale
-
-L.push = love.graphics and love.graphics.push
-L.eventPush = love.event and love.event.push
-
-L.clear = love.graphics and love.graphics.clear
-love.eventClear = love.event and love.event.clear
-
-L.setPosition = function()
-    error('Please use `L.audioSetPosition`, `L.mouseSetPosition` or `L.windowSetPosition`')
-end
-L.audioSetPosition = love.audio and love.audio.setPosition
-L.mouseSetPosition = love.mouse and love.mouse.setPosition
-L.windowSetPosition = love.window and love.window.setPosition
-
-L.isDown = function()
-    error('Please use `L.keyboardIsDown` or `L.mouseIsDown`')
-end
-L.keyboardIsDown = love.keyboard and love.keyboard.isDown
-L.mouseIsDown = love.mouse and love.mouse.isDown
-
-L.isVisible = function()
-    error('Please use `L.mouseIsVisible` or `L.windowIsVisible`')
-end
-L.windowIsVisible = love.window and love.window.isVisible
-L.mouseIsVisible = love.mouse and love.mouse.isVisible
-
-L.getPosition = function()
-    error('Please use `L.audioGetPosition`, `L.mouseGetPosition`, `L.touchGetPosition` or `L.windowGetPosition`')
-end
-L.audioGetPosition = love.audio and love.audio.getPosition
-L.mouseGetPosition = love.mouse and love.mouse.getPosition
-L.touchGetPosition = love.touch and love.touch.getPosition
-L.windowGetPosition = love.window and love.window.getPosition
 
 
 -- Extras
